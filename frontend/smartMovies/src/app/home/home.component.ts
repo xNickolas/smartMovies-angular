6a4ebb99f2f1e77e5fccaa74dfa7a5850bf111da
xNickolas <nickolsctt@gmail.com>
2021-03-27 21:15:01 -0300

    feat: style changes and slider banner from the "Get Now Playing"

--- frontend/smartMovies/src/app/home/home.component.ts
+++ frontend/smartMovies/src/app/home/home.component.ts
@@ -1,15 +1,53 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs/internal/operators/delay';
+
+import { SmartService } from '../../app/components/smart.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]),
+      transition('* => void', [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]),
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // tslint:disable-next-line: variable-name
+  movies_data: any;
+
+  current = 0;
+
+  constructor(
+    private smartService: SmartService
+  ) { }
 
   ngOnInit(): void {
+    this.getNowPlayingMovies(1);
+    this.sliderTimer();
+  }
+
+  getNowPlayingMovies(page: number) {
+    this.smartService.getNowPlaying(page)
+    .pipe(
+      delay(1000)
+    )
+    .subscribe((res: any) => {
+      this.movies_data = res.results;
+      console.dir(res);
+      console.log('PlayingNow deu certo no console', res);
+    });
+  }
+
+  sliderTimer() {
+    setInterval(() => {
+      this.current = ++this.current % this.movies_data.length;
+    }, 500000);
   }
 
 }
