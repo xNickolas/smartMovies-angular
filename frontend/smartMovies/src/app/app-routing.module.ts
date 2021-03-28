1ae5bf003267242c77d6c6a801feeb637ec3c1ac
xNickolas <nickolsctt@gmail.com>
2021-03-27 21:10:44 -0300

    feat: style changes and routes added

--- frontend/smartMovies/src/app/app-routing.module.ts
+++ frontend/smartMovies/src/app/app-routing.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { TvSeriesDetailsComponent } from './components/tv-series-details/tv-series-details.component';
+import { TvSeriesComponent } from './components/tv-series/tv-series.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
+  { path: '', component: HomeComponent,
+  children: [
+      {
+        path: '',
+        redirectTo: 'movies',
+        pathMatch: 'full'
+      },
+      {
+        path: 'movies',
+        component: MoviesComponent,
+      },
+      {
+        path: 'tv-series',
+        component: TvSeriesComponent,
+      },
+    ]
   },
+  {
+    path: 'movies/:id',
+    component: MovieDetailsComponent,
+  },
+  {
+    path: 'tv-series/:id',
+    component: TvSeriesDetailsComponent,
+  },
+
 ];
 
 @NgModule({
--- frontend/smartMovies/src/app/app.module.ts
+++ frontend/smartMovies/src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { TvSeriesComponent } from './components/tv-series/tv-series.component';
 import { HomeComponent } from './home/home.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { TvSeriesDetailsComponent } from './components/tv-series-details/tv-series-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MoviesComponent,
+    TvSeriesComponent,
+    MovieDetailsComponent,
+    TvSeriesDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatPaginatorModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
